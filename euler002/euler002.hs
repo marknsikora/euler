76b97c50165f9d37652ca56b661a9bc5a2143eac
Mark N. Sikora <marknsikora@gmail.com>
2013-02-03 20:05:15 -0500

    Use function application instead of parentheses

--- euler002/euler002.hs
+++ euler002/euler002.hs
@@ -5,7 +5,7 @@ fibs :: (Integral a) => [a]
 fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
 euler002 :: Int
-euler002 = sum (filter odd (takeWhile (<maxCount) fibs))
+euler002 = sum $ filter even $ takeWhile (<maxCount) fibs
 
 main :: IO ()
 main = do putStrLn (show euler002)
