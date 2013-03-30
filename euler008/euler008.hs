c2df48356b064531a0439f53f80e7b3783925850
Mark N. Sikora <marknsikora@gmail.com>
2013-03-30 01:33:48 -0400

    Function should be main not euler

--- euler008/euler008.hs
+++ euler008/euler008.hs
@@ -2,13 +2,13 @@ import Data.Char (digitToInt)
 
 productFold :: Num a => Int -> [a] -> [a]
 productFold _ [] = []
-productFold n all@(x:xs) = 
+productFold n all@(_:xs) = 
   if length partialList == n
     then product partialList : (productFold n xs) 
     else []
   where partialList = take n all
 
-euler008 = do
+main = do
   str <- readFile "number.txt"
   let number = map digitToInt $ concat $ lines str
   print $ maximum $ productFold 5 number
