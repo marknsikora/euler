8e3cb4d00b326e7342bb0db5639d6de23a5af318
Mark N. Sikora <marknsikora@gmail.com>
2013-04-04 22:17:12 +0200

    Add solution to problem 9

---
+++ euler009/euler009.hs
@@ -0,0 +1,18 @@
+import Control.Monad
+
+pythagoreanTriplets :: Integral a => a -> [(a,a,a)]
+pythagoreanTriplets n = do
+  c <- [1..n-2]
+  b <- [1..n-c-1]
+  let a = n - c - b
+  guard (a < b)
+  guard (c*c == a*a + b*b)
+  return (a,b,c)
+  
+tripletProduct :: Integral a => (a,a,a) -> a
+tripletProduct (a,b,c) = a * b * c
+
+euler009 :: Integral a => a
+euler009 = tripletProduct . head . pythagoreanTriplets $ 1000
+
+main = do print euler009
