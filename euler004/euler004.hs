43d075e153b888a02f4cb2ef296feeb085ae07c3
Mark N. Sikora <marknsikora@gmail.com>
2013-04-05 00:59:14 +0200

    Add solution to problem 4

---
+++ euler004/euler004.hs
@@ -0,0 +1,10 @@
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome xs = xs == (reverse xs)
+
+euler004 :: Integral a => a
+euler004 = maximum [ z | x <- [999,998..100],
+                         y <- [999,998..x],
+                         let z = x*y,
+                         isPalindrome $ show z ]
+
+main = do print euler004
