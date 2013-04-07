851004e3b1ad13a5ae478e0284962a6fcc598e54
Mark N. Sikora <marknsikora@gmail.com>
2013-04-07 02:40:18 -0400

    Add solution to problem 13

---
+++ euler013/euler013.hs
@@ -0,0 +1,9 @@
+import Data.Char (digitToInt)
+
+euler013 :: IO Integer
+euler013 = do
+  str <- readFile "numbers.txt"
+  let numbers = map read $ lines str
+  return . read . take 10 . show . sum $ numbers
+
+main = do euler013 >>= print
