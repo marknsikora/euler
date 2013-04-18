f3a7e3ef8cc5f4a2f506ca4745c8922b3f278647
Mark N. Sikora <marknsikora@gmail.com>
2013-04-17 20:28:38 -0400

    Remove redundant bracket

--- euler006/euler006.hs
+++ euler006/euler006.hs
@@ -1,7 +1,7 @@
 euler006 :: Int
 euler006 =
   let square x = x * x
-      sumOfSquares = sum $ map (square) [1..100]
+      sumOfSquares = sum $ map square [1..100]
       squareOfSum = square $ sum [1..100]
   in squareOfSum - sumOfSquares
 
