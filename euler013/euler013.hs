167253f79326289a35bae542c22cf8eabf9976ae
Mark N. Sikora <marknsikora@gmail.com>
2013-04-29 02:20:57 -0400

    Seperate function and monad

--- euler013/euler013.hs
+++ euler013/euler013.hs
@@ -1,9 +1,10 @@
+import Control.Applicative
 import Data.Char (digitToInt)
 
-euler013 :: IO Integer
-euler013 = do
-  str <- readFile "numbers.txt"
-  let numbers = map read $ lines str
-  return . read . take 10 . show . sum $ numbers
+euler013 ss =
+  let numbers = map read ss
+  in take 10 . show . sum $ numbers
 
-main = euler013 >>= print
+main = do
+  numbers <- lines <$> readFile "numbers.txt"
+  putStrLn $ euler013 numbers
