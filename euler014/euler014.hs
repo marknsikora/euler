c2400625ced5ad54e3221fff8fcaeea90339a5bd
Mark Sikora <marknsikora@gmail.com>
2014-02-28 16:50:18 -0500

    Strict fold, solves memory issues

--- euler014/euler014.hs
+++ euler014/euler014.hs
@@ -3,6 +3,13 @@ import Data.Ord
 
 import Data.MemoCombinators as Memo
 
+maximumBy' :: (a -> a -> Ordering) -> [a] -> a
+maximumBy' cmp xs = foldl1' maxBy xs
+  where
+    maxBy x y = case cmp x y of
+      GT -> x
+      _  -> y
+
 collatz :: Integer -> Integer
 collatz = Memo.integral collatz'
   where
@@ -13,7 +20,7 @@ collatz = Memo.integral collatz'
            | otherwise = (n * 3) + 1
 
 euler014 :: Integer
-euler014 = maximumBy (comparing collatz) [1..1000000]
+euler014 = maximumBy' (comparing collatz) [1..1000000]
 
 main :: IO ()
 main = print euler014
